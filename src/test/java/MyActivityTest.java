6804b902d6b46705eea7ae3d2dbaee38ea9d0c64
Christoffer Marcussen <christoffer.marcussen@bekk.no>
2013-09-29 23:26:40 +0200

    Shell for Robolectric tests. NB: remember to make sure Robolectric builds before Android

---
+++ src/test/java/MyActivityTest.java
@@ -0,0 +1,20 @@
+package test.java;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.RobolectricTestRunner;
+
+@RunWith(RobolectricTestRunner.class)
+public class MyActivityTest {
+
+    @Before
+    public void setup() {
+
+    }
+
+    @Test
+    public void should_() {
+
+    }
+}
